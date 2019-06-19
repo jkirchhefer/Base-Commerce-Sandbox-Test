c872d4790e388589392bc9f5dcd5850953161f72
Justin Kirchhefer <slickjk@cox.net>
2019-06-19 10:20:20 -0700

    Update phpreader.php

--- PHP/phpreader.php
+++ PHP/phpreader.php
@@ -1,29 +1,38 @@
 <?php
 include "BaseCommerceClient/basecommercephpsdk/index.php";
+
 //checks if transactions file exists, creates if DNE
 if (! file_exists("/home/justin/Documents/sandbox-test/transactions.txt")) {
     $file = fopen("/home/justin/Documents/sandbox-test/transactions.txt", "w");
     fwrite($file, "Format: Type,TransactionID\n");
     fclose($file);
 }
+
 //opens transactions file
 $transactionsjson = fopen("/home/justin/Documents/sandbox-test/transactions.json", "r");       
+
 //creates array for storing transactions
 $transactions = array();
+
 //populates transactions array
 while(!feof($transactionsjson)) {
     array_push($transactions, json_decode(fgets($transactionsjson)));
     $transactions = array_filter($transactions);
 }
+
 //closes transactions file
 fclose($transactionsjson);
+
 //stores length of transactions array
 $lentransactions = count($transactions);
+
 //opens transactions file
 $file = fopen("/home/justin/Documents/sandbox-test/transactions.txt", "a");
+
 //authenticates client
 $o_bcpc = new BaseCommerceClient("0014480001", "YjSbhVjTp4zv3Jvw8F6g", "C88A85467391577A4A49A832DAF2D3E6D32F6D2092267540");
 $o_bcpc->setSandbox( true );
+
 //processes transactions and stores its type and ID
 for($i=0; $i<$lentransactions; $i++) {
     if($transactions[$i]->form === "BCT") {
@@ -54,4 +63,6 @@ for($i=0; $i<$lentransactions; $i++) {
         fwrite($file, "\n");
     } 
 }
+
 //closes transactions file
+fclose($file);
