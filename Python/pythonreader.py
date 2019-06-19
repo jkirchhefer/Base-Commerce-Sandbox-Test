e393069f1b770587e0b2c4721ee7060b2f7ea845
Justin Kirchhefer <slickjk@cox.net>
2019-06-19 10:14:29 -0700

    Create pythonreader.py

---
+++ Python/pythonreader.py
@@ -0,0 +1,58 @@
+import json
+import os
+from basecommerce import *
+
+
+def main():
+    # opens transaction json file and retrieves the transactions, storing them in a list
+    with open("/home/justin/Documents/sandbox-test/transactions.json", "r") as f:
+        transactions = f.read()
+        transactions = transactions.split("\n")
+
+    # strips empty entries from the list
+    transactions = list(filter(None, transactions))
+
+    # converts json transactions to python objects
+    for x in range(len(transactions)):
+        transactions[x] = json.loads(transactions[x])
+
+    # creates/initializes file for storing transaction IDs
+    file = "/home/justin/Documents/sandbox-test/transactions.txt"
+    if not os.path.isfile(file):
+        with open(file, "a+") as f:
+            f.write("Format: Type,TransactionID\n")
+
+    # authenticates client
+    o_client = BaseCommerceClient("0014480001", "YjSbhVjTp4zv3Jvw8F6g",
+                                  "C88A85467391577A4A49A832DAF2D3E6D32F6D2092267540", True)
+
+    # iterates through transactions, processing them and storing transaction types and IDs
+    with open(file, "a") as f:
+        for transaction in transactions:
+            if transaction["form"] == "BCT":
+                bct = BankCardTransaction()
+                bct.type = transaction["type"]
+                bct.name = transaction["name"]
+                bct.card_number = transaction["number"]
+                bct.expiration_month = transaction["month"]
+                bct.expiration_year = transaction["year"]
+                bct.amount = transaction["amount"]
+                bct = o_client.process_bank_card_transaction(bct)
+                f.write("BCT," + str(bct.id) + "\n")
+
+            elif transaction["form"] == "BAT":
+                bat = BankAccountTransaction()
+                bat.type = transaction["type"]
+                bat.method = transaction["method"]
+                bat.routing_number = transaction["rt_number"]
+                bat.account_type = transaction["acct_type"]
+                bat.account_name = transaction["name"]
+                bat.account_number = transaction["acct_number"]
+                bat.amount = transaction["amount"]
+                bat.effective_date = datetime.now()
+                bat = o_client.process_bank_account_transaction(bat)
+                f.write("BAT," + str(bat.id) + "\n")
+
+
+if __name__ == "__main__":
+    main()
