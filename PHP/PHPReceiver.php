82b477cbc2814c938688bb7373a921c0bc566d0d
Justin Kirchhefer <slickjk@cox.net>
2019-06-21 16:18:06 -0700

    Update PHPReceiver.php

--- PHP/PHPReceiver.php
+++ PHP/PHPReceiver.php
@@ -4,55 +4,55 @@ include "BaseCommerceClient/basecommercephpsdk/index.php";
 
 //path variables for the transactions and statuses files
 //paths should be String
-$transactionsPath = $path_to_transactions.txt;
-$statusesPath = $path_to_statuses.csv;
+$transactions_path = $path_to_transactions.txt;
+$statuses_path = $path_to_statuses.csv;
 
 //creates/initializes statuses file if DNE
-if (! file_exists($statusesPath)) {
-    $statuses = fopen($statusesPath, "w");
+if (! file_exists($statuses_path)) {
+    $statuses = fopen($statuses_path, "w");
     fwrite($statuses, "Name,Id,Amount,Status\n");
     fclose($statuses);
 }
 
 //opens the transactions and statuses files for reading and writing, respectively
-$transactionsFile = fopen($transactionsPath, "r");
-$statuses = fopen($statusesPath, "a");
+$transactions_file = fopen($transactions_path, "r");
+$statuses = fopen($statuses_path, "a");
 
 //iteratespast the header information
-fgets($transactionsFile);        
+fgets($transactions_file);        
 
 //creates array for transaction types and ids
 $transactions = array();
 
 //populates array of transaction types and ids, filtering empty entries
-while(!feof($transactionsFile)) {
-    $transactionInfo = explode(",", fgets($transactionsFile));
-    $transactionInfo = array_filter($transactionInfo);
-    array_push($transactions, $transactionInfo);
+while(!feof($transactions_file)) {
+    $transaction_info = explode(",", fgets($transactions_file));
+    $transaction_info = array_filter($transaction_info);
+    array_push($transactions, $transaction_info);
     $transactions = array_filter($transactions);
 }
 
 //closes transactions file
-fclose($transactionsFile);
+fclose($transactions_file);
 
 //authenticates client
 //credentials should be string
-$o_bcpc = new BaseCommerceClient($username, $password, $key);
-$o_bcpc->setSandbox( true );
+$client = new BaseCommerceClient($username, $password, $key);
+$client->setSandbox(true);
 
 //gets transaction statuses and stores it in the statuses file
-foreach ($transactions as $transactionInfo) {
-    $type = $transactionInfo[0];
-    $id = intval($transactionInfo[1]);
+foreach ($transactions as $transaction_info) {
+    $type = $transaction_info[0];
+    $id = intval($transaction_info[1]);
             
     if($type === "BCT") {
-        $transaction = $o_bcpc->getBankCardTransaction($id);
+        $transaction = $client->getBankCardTransaction($id);
         $name = $transaction->getCardName();
         $amount = $transaction->getAmount();
         $status = $transaction->getStatus();
         fwrite($statuses, "$name,$id,$amount,$status\n");
     } elseif($type === "BAT") {
-        $transaction = $o_bcpc->getBankAccountTransaction($id);
+        $transaction = $client->getBankAccountTransaction($id);
         $name = $transaction->getAccountName();
         $amount = $transaction->getAmount();
         $status = $transaction->getStatus();
@@ -64,5 +64,5 @@ foreach ($transactions as $transactionInfo) {
 fclose($statuses);
 
 //echos session ID for later reference
-$sessionID = $o_bcpc->getLastSessionId();
+$sessionID = $client->getLastSessionId();
 echo "Session ID: $sessionID";
