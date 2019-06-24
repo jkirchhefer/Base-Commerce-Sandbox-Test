0c35ac0696538b5f9a334abe3c47434b9dcc582f
Justin Kirchhefer <slickjk@cox.net>
2019-06-24 09:44:53 -0700

    Update PythonReceiver.py

--- Python/PythonReceiver.py
+++ Python/PythonReceiver.py
@@ -25,26 +25,26 @@ def main():
         transactions[transactions_position][1] = int(transactions[transactions_position][1])
 
     # creates/initializes statuses file if DNE
-    if not os.path.isfile(statuses_path):
-        with open(statuses_path, "a") as transactions_writer:
-            transactions_writer.write("Name,ID,Amount,Status\n")
+    if not os.path.isfile(statuses_file_path):
+        with open(statuses_file_path, "a") as status_writer:
+            status_writer.write("Name,ID,Amount,Status\n")
 
     # authenticates client
     # credentials should be String
     client = BaseCommerceClient($username, $password, $key, True)
 
     # gets the transaction statuses depending on type and id and stores them in the statuses file
-    with open(statuses_file_path, "a") as transactions_writer:
-        for transaction in transactions:
-            if transaction[0] == "BCT":
-                transactions_writer = client.get_bank_card_transaction(transaction[1])
-                name = transactions_writer.name
-            elif transaction[0] == "BAT":
-                transactions_writer = client.get_bank_account_transaction(transaction[1])
-                name = transactions_writer.account_name
-            amount = transactions_writer.amount
-            status = transactions_writer.status
-            transactions_writer.write(name + "," + str(transaction[1]) + "," + str(amount) + "," + status + "\n")
+    with open(statuses_file_path, "a") as status_writer:
+        for transaction_info in transactions:
+            if transaction_info[0] == "BCT":
+                transaction = client.get_bank_card_transaction(transaction_info[1])
+                name = transaction.name
+            elif transaction_info[0] == "BAT":
+                transaction = client.get_bank_account_transaction(transaction_info[1])
+                name = transaction.account_name
+            amount = transaction.amount
+            status = transaction.status
+            status_writer.write(name + "," + str(transaction_info[1]) + "," + str(amount) + "," + status + "\n")
 
     # prints session ID for later reference
     print("Session ID: " + client.session_id)
