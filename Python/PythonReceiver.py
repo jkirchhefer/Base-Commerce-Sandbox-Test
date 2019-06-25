9b644114a0f6888fa2a48a72d3d496eb8b53fd4a
Justin Kirchhefer <slickjk@cox.net>
2019-06-25 16:05:36 -0700

    Update PythonReceiver.py

--- Python/PythonReceiver.py
+++ Python/PythonReceiver.py
@@ -19,7 +19,7 @@ def main():
     # removes blank entries
     transactions = list(filter(None, transactions))
 
-    # filters transaction type and id
+    # filters transaction form and ID
     for transactions_position in range(len(transactions)):
         transactions[transactions_position] = transactions[transactions_position].split(",")
         transactions[transactions_position][1] = int(transactions[transactions_position][1])
@@ -33,15 +33,15 @@ def main():
     # credentials should be String
     client = BaseCommerceClient($username, $password, $key, True)
 
-    # gets the transaction statuses depending on type and id and stores them in the statuses file
+    # gets the transaction statuses depending on form and ID and stores them in the statuses file
     with open(statuses_file_path, "a") as status_writer:
         for transaction_info in transactions:
             transaction_id = transaction_info[1]
-            transaction_type = transaction_info[0]
-            if transaction_type == "BCT":
+            transaction_form = transaction_info[0]
+            if transaction_form == "BCT":
                 transaction = client.get_bank_card_transaction(transaction_id)
                 name = transaction.name
-            elif transaction_type == "BAT":
+            elif transaction_form == "BAT":
                 transaction = client.get_bank_account_transaction(transaction_id)
                 name = transaction.account_name
             amount = transaction.amount
